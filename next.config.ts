cc8a9d6bf37e1b524c5a152c49e8d406838a81f0
{dyk0224} <epn0224@gmail.com>
2025-06-30 01:17:17 +0900

    chore: 패키지 매니저 수정

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,7 @@
-import type { Configuration } from "webpack";
-import type { WebpackConfigContext } from "next/dist/server/config-shared";
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config: Configuration, context: WebpackConfigContext): Configuration {
-    const rules = config.module?.rules;
-
-    if (Array.isArray(rules)) {
-      const fileLoaderRule = rules.find(
-        (rule): rule is { [key: string]: any } =>
-          typeof rule === "object" &&
-          rule !== null &&
-          "test" in rule &&
-          rule.test instanceof RegExp &&
-          rule.test.test(".svg")
-      );
-
-      if (fileLoaderRule) {
-        fileLoaderRule.exclude = /\.svg$/i;
-      }
-    }
-
-    config.module?.rules?.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
-    });
-
-    return config;
+  images: {
+    domains: ["encrypted-tbn0.gstatic.com"],
   },
 };
 
